b62b13adff3cd6842ae41ac2c343e59fb23b6c86
Adarsh Modh <adarsh.modh@gmail.com>
2026-03-19 16:12:21 -0400

    Refactor weights and bias variables in classifier head

--- vgg/vgg-classifier-head.py
+++ vgg/vgg-classifier-head.py
@@ -11,14 +11,15 @@ def vgg_classifier(features: np.ndarray, num_classes: int = 1000) -> np.ndarray:
     B, C, H, W = features.shape
     flat_feats = features.reshape(B, C*H*W)
     
-    weights = np.random.randn(C*H*W, 4096) * 0.01
-    bias = np.ones(4096)*0.5
-    fc1 = relu(flat_feats @ weights + bias)
+    W1 = np.random.randn(C*H*W, 4096) * 0.01
+    B1 = np.ones(4096)*0.5
+    fc1 = relu(flat_feats @ W1 + B1)
     
-    weights = np.random.randn(4096, 4096) * 0.01
-    fc2 = relu(fc1 @ weights + bias)
+    W2 = np.random.randn(4096, 4096) * 0.01
+    B2 = np.ones(4096)*0.5
+    fc2 = relu(fc1 @ W2 + B2)
     
-    weights = np.random.randn(4096, num_classes) * 0.01
-    bias = np.ones(num_classes)*0.5
-    fc3 = fc2 @ weights + bias
+    W3 = np.random.randn(4096, num_classes) * 0.01
+    B3 = np.ones(num_classes)*0.5
+    fc3 = fc2 @ W3 + B3
     return fc3
